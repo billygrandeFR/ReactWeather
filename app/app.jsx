f8097e3ad0768cbc1684739d69f98da8f975c1ac
BillyGrande <kevinsheridancv@gmail.com>
2017-03-10 15:08:42 +0100

    bs

--- app/app.jsx
+++ app/app.jsx
@@ -13,12 +13,13 @@ $(document).foundation();
 require('style!css!./styles/app.css')
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Main}>
-    <Route path="about" component={About} />
-    <Route path="examples" component={Examples} />
-    <IndexRoute component={Weather} />
-    </Route>
-  </Router>,
+  // <Router history={hashHistory}>
+  //   <Route path="/" component={Main}>
+  //   <Route path="about" component={About} />
+  //   <Route path="examples" component={Examples} />
+  //   <IndexRoute component={Weather} />
+  //   </Route>
+  // </Router>,
+  <h1>Testing BlackSee</h1>,
   document.getElementById('app')
 );
